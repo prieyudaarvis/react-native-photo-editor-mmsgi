143d6331fa14b35473402b1ac07ceb96bcb666ce
Prieyuda Akadita S <ydhnwb-pc@pop-os.localdomain>
2022-11-08 09:39:07 +0700

    remove android pkg

--- android/src/main/java/com/reactnativephotoeditor/activity/filters/FilterListener.java
+++
@@ -1,7 +0,0 @@
-package com.reactnativephotoeditor.activity.filters;
-
-import ja.burhanrashid52.photoeditor.PhotoFilter;
-
-public interface FilterListener {
-    void onFilterSelected(PhotoFilter photoFilter);
-}
